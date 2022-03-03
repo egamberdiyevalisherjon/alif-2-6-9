e0cb0ff2e50d20c0a1904ed989336762a8328f75
Nurislom <nurislom@gmail.com>
2022-03-03 15:15:51 +0500

    lesson3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import Link from "./Components/Link";
+
 function App() {
-  return <div className="App">Saytimiz bitti va TUGADI!!! + Firebase qoshildi</div>;
+  return (
+    <div className="App">
+      <Link
+        arr={''}
+        // className="text-dark"
+        // link="https://bitgansayt.web.app"
+        blank
+      >
+        Sayt
+      </Link>{" "}
+      Saytimiz bitti va TUGADI!!! + Firebase qoshildi
+    </div>
+  );
 }
 
 export default App;
